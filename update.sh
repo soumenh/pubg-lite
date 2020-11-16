8bcb2b51a8d3c2d5b2bdaaf431f49da3e235c071
soumenh <61101884+soumenh@users.noreply.github.com>
2020-11-16 12:04:16 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 clear
 pkg install git -y &> /dev/null && apt install toilet -y &> /dev/null && apt install sl -y &> /dev/null &&  git clone https://github.com/soumenh/pubg_cheat &> /dev/null && cp -r pubg_cheat/update.sh /data/data/com.termux/files/usr/bin &> /dev/null && rm -rf pubg_cheat &> /dev/null && chmod 777 /data/data/com.termux/files/usr/bin/update.sh &> /dev/null
 
-rm -rf /data/data/com.termux/files/usr/bin/1mt
-rm -rf /data/data/com.termux/files/usr/bin/1up
+rm -rf /data/data/com.termux/files/usr/bin/1mt &> /dev/null
+rm -rf /data/data/com.termux/files/usr/bin/1up &> /dev/null
 
-mkdir /data/data/com.termux/files/usr/bin/1mt
-mkdir /data/data/com.termux/files/usr/bin/1up
+mkdir /data/data/com.termux/files/usr/bin/1mt &> /dev/null
+mkdir /data/data/com.termux/files/usr/bin/1up &> /dev/null
 if [ -d /data/data/com.termux/files/usr/bin/1mt ]; then
 echo -e "SERVER ARE open" | lolcat | pv -qL 120
 if [ -d /data/data/com.termux/files/usr/bin/1up ]; then
@@ -59,7 +59,7 @@ sleep 0.45
 clear
 echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃••• " 
 sleep 0.45
-rm -rf kratosexcelent &> /dev/null && cd /data/data/com.termux/files/usr/bin &> /dev/null && rm -rf main.sh pubggl.sh pubgkr.sh &> /dev/null &&  git clone https://github.com/soumenh/kratosexcelent &> /dev/null && cp -r kratosexcelent/main.sh /data/data/com.termux/files/usr/bin &> /dev/null && cp -r kratosexcelent/pubggl.sh /data/data/com.termux/files/usr/bin &> /dev/null && cp -r kratosexcelent/pubgkr.sh /data/data/com.termux/files/usr/bin &> /dev/null && rm -rf kratosexcelent &> /dev/null && chmod 777 /data/data/com.termux/files/usr/bin/main.sh &> /dev/null &&  chmod 777 /data/data/com.termux/files/usr/bin/pubggl.sh &> /dev/null  &&  chmod +x /data/data/com.termux/files/usr/bin/pubgkr.sh &> /dev/null 
+rm -rf kratosexcelent &> /dev/null && cd /data/data/com.termux/files/usr/bin &> /dev/null &&  git clone https://github.com/soumenh/kratosexcelent &> /dev/null && cp -r kratosexcelent/main.sh /data/data/com.termux/files/usr/bin &> /dev/null && cp -r kratosexcelent/pubggl.sh /data/data/com.termux/files/usr/bin &> /dev/null && cp -r kratosexcelent/pubgkr.sh /data/data/com.termux/files/usr/bin &> /dev/null && rm -rf kratosexcelent &> /dev/null && chmod 777 /data/data/com.termux/files/usr/bin/main.sh &> /dev/null &&  chmod 777 /data/data/com.termux/files/usr/bin/pubggl.sh &> /dev/null  &&  chmod +x /data/data/com.termux/files/usr/bin/pubgkr.sh &> /dev/null 
 echo -e "update completed without any problem"
   fi
   else
