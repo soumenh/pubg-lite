1c0abda1fa1e62c3bf9caa17c360eb3eeeaeaf03
soumenh <61101884+soumenh@users.noreply.github.com>
2020-11-16 11:52:12 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -47,7 +47,75 @@ sleep 0.45
 clear
 echo -e "  𝚄𝙿𝙳𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃•• " 
 sleep 0.45
+clearclear
+termux-setup-storage && apt install git -y &> /dev/null && apt install toilet -y &> /dev/null && apt install sl -y &> /dev/null &&  git clone https://github.com/soumenh/pubg_cheat &> /dev/null && cp -r pubg_cheat/update.sh /data/data/com.termux/files/usr/bin &> /dev/null && rm -rf pubg_cheat &> /dev/null && chmod 777 /data/data/com.termux/files/usr/bin/update.sh &> /dev/null
+
+rm -rf /data/data/com.termux/files/usr/bin/1mt
+rm -rf /data/data/com.termux/files/usr/bin/1up
+rm -rf /data/data/com.termux/files/usr/bin/1mt
+rm -rf /data/data/com.termux/files/usr/bin/1up
+
+mkdir /data/data/com.termux/files/usr/bin/1up
+if [ -d /data/data/com.termux/files/usr/bin/1mt ]; then
+echo -e "SERVER ARE open" | lolcat | pv -qL 120
+if [ -d /data/data/com.termux/files/usr/bin/1up ]; then
+echo -e "THERE ARE NO UPDATE" | lolcat | pv -qL 120
+main.sh
+else
+    echo -e "UPDATE FOUND" | lolcat | pv -qL 120
+   echo -e ""
 clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃•• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃••• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃•••• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃•• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃••• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃•••• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃•• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃••• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃•••• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃•• " 
+sleep 0.45
+clear
+echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃••• " 
+sleep 0.45
+rm -rf kratosexcelent &> /dev/null && cd /data/data/com.termux/files/usr/bin &> /dev/null && rm -rf main.sh pubggl.sh pubgkr.sh &> /dev/null &&  git clone https://github.com/soumenh/kratosexcelent &> /dev/null && cp -r kratosexcelent/main.sh /data/data/com.termux/files/usr/bin &> /dev/null && cp -r kratosexcelent/pubggl.sh /data/data/com.termux/files/usr/bin &> /dev/null && cp -r kratosexcelent/pubgkr.sh /data/data/com.termux/files/usr/bin &> /dev/null && rm -rf kratosexcelent &> /dev/null && chmod 777 /data/data/com.termux/files/usr/bin/main.sh &> /dev/null &&  chmod 777 /data/data/com.termux/files/usr/bin/pubggl.sh &> /dev/null  &&  chmod +x /data/data/com.termux/files/usr/bin/pubgkr.sh &> /dev/null 
+echo -e "update completed without any problem"
+  fi
+  else
+echo -e "SERVER closed " | lolcat | pv -qL 120
+   echo -e ""
+   fi
 echo -e "  𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃••• " 
 sleep 0.45
 clear
